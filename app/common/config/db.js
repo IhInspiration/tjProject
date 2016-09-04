5dc78e5c8bb68daa8f244e4b1556b6b132bfaf4b
jackwang <wdmzjjm@163.com>
2016-09-04 20:34:25 +0800

    use mongo

--- app/common/config/db.js
+++ app/common/config/db.js
@@ -4,19 +4,42 @@
  * @type {Object}
  */
 module.exports = {
-  type: 'mysql',
+  type: 'mongo',
   adapter: {
     mysql: {
       host: '127.0.0.1',
       port: '',
-      database: '',
-      user: '',
+      database: 'tjProject',
+      user: 'root',
       password: '',
       prefix: 'think_',
-      encoding: 'utf8'
+      encoding: 'utf8',
+      nums_per_page: 10,
+      log_sql: true,
+      log_connect: true,
+      cache: {
+        on: true,
+        type: "",
+        timeout: 3600
+      }
     },
     mongo: {
-
+      host: '127.0.0.1',
+      port: '27017',
+      prefix: 'think_',
+      database: 'tjProject',
+      encoding: 'utf8',
+      nums_per_page: 10,
+      log_sql: true,
+      log_connect: true,
+      cache: {
+        on: true,
+        type: "",
+        timeout: 3600
+      },
+      options: {
+        //authSource: 'admin'
+      }
     }
   }
 };
