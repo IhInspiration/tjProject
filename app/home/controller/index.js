91569442d9ce7e276b6835e6d2c9930e8269da02
jackwang <wdmzjjm@163.com>
2016-09-04 20:36:40 +0800

    add controller for index and article pages

--- app/home/controller/index.js
+++ app/home/controller/index.js
@@ -8,12 +8,29 @@ module.exports = think.controller(Base, {
    * @return {Promise} []
    */
   indexAction: function(self){
-    //auto render template file index.html
-    var data = {
-      name: 'JackWang',
-      content: "Hello,World!"
-    };
-    self.assign("data", data);
     return self.display();
+  },
+  addAction: function(self){
+    var http = this.http,
+        getData = http._post,
+        url;
+    this.model("article").add({
+      title: getData.title,
+      author: getData.author,
+      time: new Date().toLocaleString(),
+      content: getData.content
+    }).then(function(data){
+      console.log(data);
+      url = '/index/article?id=' + data;
+      console.log(url);
+      http.redirect(url);
+    });
+  },
+  articleAction: function(self){
+    var id = this.http.get("id");
+    this.model("article").where({_id: id}).find().then(function(data){
+      self.assign("data", data);
+      return self.display();
+    });
   }
 });
