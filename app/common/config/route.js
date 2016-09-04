674b0e4bd8ca6ba6ae9b597160856893844160b9
jackwang <wdmzjjm@163.com>
2016-09-04 20:34:55 +0800

    annotation

---
+++ app/common/config/route.js
@@ -0,0 +1,12 @@
+/**
+ * Created by lenovo on 2016/8/25.
+ * @author jackwang <wdmzjjm@163.com>
+ * @blog http://www.notalent.cn
+ */
+module.exports = [
+    ["规则1", "需要识别成的pathname"],
+    ["规则2", {
+        get: "get请求下需要识别成的pathname",
+        post: "post请求下需要识别成的pathname"
+    }]
+];
